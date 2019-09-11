81a9ff1780ef37c2be9ab95d78d07d8974818faf
Alexandru Smeu <alexandru.smeu@pss.ro>
2019-09-11 13:14:39 +0300

    Create AddresRest response model

---
+++ src/main/java/ro/pss/asm/tutorials/spring/ui/model/response/AddressRest.java
@@ -0,0 +1,49 @@
+package ro.pss.asm.tutorials.spring.ui.model.response;
+
+public class AddressRest {
+	String addressId;
+	String city;
+	String country;
+	String street;
+	String postalCode;
+	String type;
+	
+	public String getAddressId() {
+		return addressId;
+	}
+	public void setAddressId(String addressId) {
+		this.addressId = addressId;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getPostalCode() {
+		return postalCode;
+	}
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	
+}
